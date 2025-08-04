a995bfff8c64f5b175ceaa5ee1a7422ad8d116f2
simar-rekhi <srekhi.simar@gmail.com>
2025-08-03 22:52:59 -0500

    Add boilerplate; usable from chap 3 thru 5

---
+++ practice/practice/ch1.asm
@@ -0,0 +1,25 @@
+; BOILERPLATE FOR CODING ASSIGNMENTS
+
+; Author: Simar Rekhi
+; Date: mm-dd-yyyy
+; Description:
+COMMENT !
+	give a brief of the approach you will use to solve this question
+!
+
+; ENVIRONMENT SETUP
+.386
+.model flat, stdcall
+.stack 4096
+ExitProcess PROTO, dwExitCode: DWORD
+
+.data
+	; in case you have any memory storage 
+
+.code
+main PROC
+	
+	; actual code resides here
+
+main ENDP
+END main
