50898b802cc3b445d346a0e5373c82df4a986454
simar-rekhi <srekhi.simar@gmail.com>
2025-08-04 22:03:59 -0500

    intro to Irvine32 lib

--- practice/practice/ch5.asm
+++ practice/practice/ch5.asm
@@ -1,33 +1,20 @@
-.386
-.model flat, stdcall
-.stack 4096
-ExitProcess PROTO, dwExitCode: DWORD
+INCLUDE Irvine32.inc
 
-.data
-	aName BYTE "Abraham Lincoln",0
-	nameSize = ($ - aName) - 1
+; Author:
+; Date:
+; Description
+COMMENT !
+	using the irvine library: 
+	clear the screen, delay prog by 500 ms, dump regs and flags
+!
 
 .code
 main PROC
-	; push the name on the stack
-	mov ecx, nameSize
-	mov esi, 0
+	call Clrscr
+	mov eax, 500
+	call Delay
+	call DumpRegs
 
-L1: movzx eax, aName[esi]
-	push eax
-	inc esi
-	loop L1
-
-; start popping in reverse order
-	mov ecx, nameSize
-	mov esi, 0
-
-L2: pop eax
-	mov aName[esi], al
-	inc esi
-	loop L2
-
-	INVOKE ExitProcess,0
-
+	exit
 main ENDP
 END main
